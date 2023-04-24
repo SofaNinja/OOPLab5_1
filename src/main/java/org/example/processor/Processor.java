c0396eac664d852c5b58d803bbab7455652835e4
SofaNinja <92326441+SofaNinja@users.noreply.github.com>
2023-04-24 09:27:09 +0300

    123

--- src/main/java/org/example/processor/Processor.java
+++ src/main/java/org/example/processor/Processor.java
@@ -1,12 +1,10 @@
 package org.example.processor;
-public class Processor implements ProcessArray{
-    @Override
+
+public class Processor implements ProcessArray {
     public int calculate(int[] array) {
         int max = array[0];
-        for (int i = 1; i < array.length; i++) {
-            if(array[i] % 2 != 0){
+        for (int i = 1; i < array.length; i += 2) {
                 if(array[i] > max) max = array[i];
-            }
         }
         return max;
     }
